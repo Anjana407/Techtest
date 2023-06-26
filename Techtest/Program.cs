65bf6283b4f4202363f53b87f67dfdea3420e04c
Anjana407 <134396263+Anjana407@users.noreply.github.com>
2023-06-26 16:13:58 +0530

    Update Program.cs

--- Techtest/Program.cs
+++ Techtest/Program.cs
@@ -6,27 +6,7 @@ using Techtest;
 
 class Program
 {
-    // Function that returns true if x < y
-    // according to the new alphabetical order
-    private static int Compare(string x, string y)
-    {
-        var str1 = x.ToCharArray();
-        var str2 = y.ToCharArray();
-        Array.Sort(str1);
-        Array.Sort(str2);
-        int minSize = Math.Min(str1.Length, str2.Length);
-        for (int i = 0; i < minSize; i++)
-        {
-            if (str1[i] == str2[i])
-                continue;
-            else if (str1[i] > str2[i])
-                return 1;
-            else
-                return -1;
-        }
-        return 0;
-    }
-
+    
     // Driver code
     static void Main(string[] args)
     {
